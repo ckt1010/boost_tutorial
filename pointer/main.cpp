5181ade7b7dd980e9ef46e528cc24fc6f9a1f2c4
kaitaocheng <kaitaocheng@ecarx.com.cn>
2020-02-07 20:48:23 +0800

    add no-copy test in pointer

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -1,24 +1,39 @@
+#include <boost/scoped_array.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/weak_ptr.hpp>
-#include <boost/scoped_array.hpp>
 #include <iostream>
 
 using namespace std;
 using namespace boost;
 
-class A {
+class A_parent {
+  public:
+    A_parent() { cout << "in A_parent construct;\n"; };
+    ~A_parent() { cout << "in A_parent deconstruct;\n"; };
+
+  private:
+    A_parent(A_parent &a) { cout << "in A_parent copy construct;\n"; };
+    A_parent operator=(A_parent &a) {
+        cout << "in A_parent equal construct;\n";
+        return *this;
+    };
+};
+
+class A: public A_parent {
   private:
   public:
     A();
     ~A();
+    A operator=(A &a) {
+        cout << "in A_parent equal construct;\n";
+        return *this;
+    };
 };
 
-A::A() {
-    cout << "A constructed\n";
-}
+A::A() : A_parent() { cout << "in A constructed\n"; }
 
-A::~A() {cout << "A deconstructed\n";}
+A::~A() { cout << "in A deconstructed\n"; }
 
 int main() {
     cout << "scoped_ptr:\n";
@@ -32,4 +47,10 @@ int main() {
     cout << "\nshared_ptr:\n";
     boost::shared_ptr<A> shptr(new A);
     boost::weak_ptr<A> wptr(shptr);
+
+    cout << "\nCopy test:\n";
+    A copy1, copy2;
+    copy1 = copy2;
+
+    
 }
